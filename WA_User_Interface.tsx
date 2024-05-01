fa7c70025dd70ed34efe029dbfd19dbf71e21f27
Chardonneaur <contact@ronan-chardonneau.fr>
2024-05-01 18:56:27 +0200

    new test

--- WA_User_Interface.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="WA_User_Interface" tilewidth="32" tileheight="32" tilecount="273" columns="13">
- <image source="tilesets/WA_User_Interface.png" width="416" height="672"/>
-</tileset>
